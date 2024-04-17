fadda0eafa0324297b9da27266894228252a8229
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-04-17 11:40:04 +0800

    feat: Cascade delete for add ons when trip is deleted

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-    has_many :add_ons
+    has_many :add_ons, dependent: :delete_all # cascade delete
 
     def as_json(options = {})
         super(include: { add_ons: {  } })
